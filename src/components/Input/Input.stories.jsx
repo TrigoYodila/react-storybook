9f66268b8ba1fe040e45f5f036eba77a5d9521de
Trigo <trigoyodila1996@gmail.com>
2024-04-11 14:37:48 +0200

    nested histories to a folder

--- src/components/Input/Input.stories.jsx
+++ src/components/Input/Input.stories.jsx
@@ -1,13 +1,13 @@
 import { Input } from "./Input";
 
 export default {
-    title:"Input",
+    title:"Form/Input",
     component:Input
 }
 
 export const Small = () => <Input type='text' size='small' style='h-8' placeholder="Small size" />
 
-export const Medium = () => <Input type='text' size='medium' style='h-10'placeholder="Medium size" />
+export const Medium = () => <Input type='password' size='medium' style='h-10'placeholder="Medium size" />
 
 export const Large = {
     render : () => <Input type='text' size='large' style='h-12' placeholder="Large size"/>
