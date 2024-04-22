5731d1c30950dcb521691081986f6b3f0e521222
Trigo <trigoyodila1996@gmail.com>
2024-04-22 15:20:28 +0200

    custum btn component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -12,3 +12,19 @@ Button.propTypes = {
   variant: PropTypes.string.isRequired,
   children: PropTypes.string
 }
+
+
+export const Btn = ({types,label,bgColor}) => {
+  const styles = types === "primary" ? `text-white bg-${bgColor}` : `bg-transparent border-2 border-${bgColor} text-${bgColor}`
+  return (
+    <button  className={`py-2 px-4 text-center inline-block font-md rounded-md cursor-pointer w-fit ${styles}`}>
+      {label}
+    </button>
+  )
+}
+
+Btn.propTypes = {
+  types:PropTypes.bool,
+  label : PropTypes.string,
+  bgColor:PropTypes.string
+}
--- src/components/Button/Button.stories.jsx
+++ src/components/Button/Button.stories.jsx
@@ -1,19 +1,28 @@
-import { Button } from "./Button"
+// import { Button } from "./Button"
+import { Btn } from "./Button"
 
 export default {
     title:"Design System/Atoms/Button",
-    component:Button
+    component:Btn
 }
 
-export const Primary = () => <Button variant='bg-primary'>Primary</Button>
+// export const Primary = () => <Button variant='bg-primary'>Primary</Button>
 
-export const Secondary = () => <Button variant='bg-secondary' color='text-black'>Secondary</Button>
+// export const Secondary = () => <Button variant='bg-secondary' color='text-black'>Secondary</Button>
 
 //otherwise
-export const Success = {
-    render : () => <Button variant='bg-success'>Success</Button>
+// export const Success = {
+//     render : () => <Button variant='bg-success'>Success</Button>
+// }
+
+// export const Danger = {
+//     render : () => <Button variant='bg-danger'>Danger</Button>
+// }
+
+export const Primary = {
+    render:() => <Btn types="primary" label="Primary" bgColor="primary" />
 }
 
-export const Danger = {
-    render : () => <Button variant='bg-danger'>Danger</Button>
+export const Secondary = {
+    render:() => <Btn types="secondary" label="Secondary" bgColor="success" />
 }
